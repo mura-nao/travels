7679fd7dc1f21f10bc7c764e3e5753dd70a99156
naoshi.murayama <go_convination9@yahoo.co.jp>
2019-12-22 12:59:19 +0000

    Toppagecontroller2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,5 @@
 class ToppagesController < ApplicationController
   def index
-    @prefecture = Prefecture.new(name:"宮城県")
-    @prefecture = Prefecture.new(name:"山形県")
-    @prefecture = Prefecture.new(name:"福島県")
     @prefectures = Prefecture.all
   end
 end
